1a25ec31da635f64fde5a922267866905c946673
Rishabhveer singh <44227973+rishabhkailey@users.noreply.github.com>
2020-10-28 10:28:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const port = 5000;
 app.use(bodyParser.json());
 
 const Sentry = require('@sentry/node');
-Sentry.init({ dsn: 'https://9e94fae3064e4c5c9d217a4cc47ed3ae@o409436.ingest.sentry.io/5281897' });
+Sentry.init({ 
+    environment: "production",
+    dsn: 'https://9e94fae3064e4c5c9d217a4cc47ed3ae@o409436.ingest.sentry.io/5281897'
+});
 
 app.get('/', (req, res)=> {
     res.status(200).send("hello")
